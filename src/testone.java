d919d87e262fdb7831d4b910c3bb366421de0a17
MuratOldman <muratkara15102@gmail.com>
2023-04-29 22:17:41 -0400

    I added bigSmall method

--- src/testone.java
+++ src/testone.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class testone {
     // some comment here
     //another comment
@@ -5,5 +9,33 @@ public class testone {
         // code here
 
         // another one
+
+
+        System.out.println(bigSmall(new ArrayList<>(Arrays.asList(5,2,0,7,15,1,99,-21))));
     }
-}
+
+    public static ArrayList<Integer> bigSmall (ArrayList<Integer> list){
+        Collections.sort(list);
+        System.out.println(list);
+        ArrayList<Integer> result=new ArrayList<>();
+
+        int j=list.size()-1;
+        for (int i = 0; i < list.size()/2+1;) {
+            if (i<j) {
+                result.add(list.get(i));
+                result.add(list.get(j));
+            } else if(i==j) {
+                result.add(list.get(i));
+                return result;
+            }
+            j--;
+            i++;
+        }
+        return  result;
+        }
+
+    }
+
+
+
+
